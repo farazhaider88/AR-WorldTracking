557a319342dc75e3ec01f1d87989d7773fe33d09
FarazHaider <farazhaider88@yahoo.com>
2020-08-17 15:41:04 +0500

    Random Positions

--- World Tracking/World Tracking/ViewController.swift
+++ World Tracking/World Tracking/ViewController.swift
@@ -24,10 +24,15 @@ class ViewController: UIViewController {
     @IBAction func addAction(_ sender: Any) {
         
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0, 0, -0.3)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x, y, z)
         self.sceneView.scene.rootNode.addChildNode(node)
+        self.sceneView.autoenablesDefaultLighting = true
     }
     
     @IBAction func resetAction(_ sender: Any) {
@@ -42,5 +47,9 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration, options: [.resetTracking,.removeExistingAnchors])
     }
     
+    func randomNumbers(firstNum : CGFloat, secondNum: CGFloat) -> CGFloat{
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum,secondNum)
+        
+    }
 }
 
