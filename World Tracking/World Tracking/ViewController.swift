7335a4f4df4e73aede05908ca8e0323459b84989
FarazHaider <farazhaider88@yahoo.com>
2020-08-17 15:18:35 +0500

    XYZ axis

---
+++ World Tracking/World Tracking/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Faraz Haider on 17/08/2020.
+//  Copyright © 2020 Faraz Haider. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+    }
+
+    @IBAction func addAction(_ sender: Any) {
+        
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0.2, 0, 0 )
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+}
+
